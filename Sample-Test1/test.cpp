388784ab0297e43f7cfa6cb7fa2721ed359597c8
JinSeok Oh <jinseok0.oh@samsung.com>
2023-07-07 10:05:51 +0900

    First Commit

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../Mocking3/DeviceDriver.cpp"
+
+using namespace std;
+using namespace testing;
+
+
+TEST(TestCaseName, TestName) {
+	EXPECT_THAT(1, Eq(1));
+
+}
